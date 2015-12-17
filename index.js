a3c090098f14ba13931dec318123eb1fe95ec2ca
FranciscoP <franciscopresencia@hotmail.com>
2015-12-17 03:44:52 +0100

    Cleaned up old code

--- index.js
+++ index.js
@@ -1,43 +1,8 @@
 var asyn = require('async');
 
-// module.exports = function(callback, arg, autopipe){
-//   
-//   var callbacks = [];
-//   
-//   // Add it as the first parameter to the callback
-//   // The first parameter has no data to send back, so we mock it
-//   if (callback && typeof callback === 'function') {
-//     callbacks.push(asyn.apply(callback, arg, {}));
-//   }
-//   
-//   var response = {
-//     pipe: repipe.bind(this, callbacks),
-//     end: end.bind(this, callbacks)
-//   };
-//   
-//   // The first one behaves differently so we re-define it
-//   function repipe(callbacks, callback, arg, autopipe){
-//     if (callback && typeof callback === 'function'){
-//       callbacks.push(asyn.apply(callback, arg));
-//     }
-//     
-//     //response.end = response.end.bind(this, callbacks);
-//     return response;
-//   }
-//   
-//   function end(callbacks, callback){
-//     asyn.waterfall(callbacks, callback);
-//   }
-//   
-//   return response;
-// };
-
-
 
 function wrap(callback, arg){
-  if (callback) {
-    return asyn.apply(callback, arg || {});
-  }
+  return callback ? asyn.apply(callback, arg) : false;
 }
 
 var pipe = function(callback, arg, autopipe){
@@ -51,18 +16,14 @@ var pipe = function(callback, arg, autopipe){
   }
   
   // Create array if nonexistent and push calback, stackoverflow.com/a/14614169
-  //  this.callbacks = (this.callbacks || []).concat(wrap(callback, arg));
   return this.add(callback, arg);
 };
 
 pipe.prototype.callbacks = [];
 
+// Add a callback to the stack
 pipe.prototype.add = function(callback, arg){
   if (callback) this.callbacks = this.callbacks.concat(wrap(callback, arg));
-  //this.callbacks = this.callbacks;
-  //if (callback) this.callbacks.push(wrap(callback, arg));
-  //if (callback) Array.prototype.push.apply(this.callbacks, wrap(callback, arg));
-  //if (callback) this.callbacks.push(wrap(callback, arg));
   return this;
 };
 
